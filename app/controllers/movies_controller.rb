a3834cf6198744a40b906f21213c553aefdb4cbc
Chen-Chien Chung <chung@tamu.edu>
2016-10-18 20:54:14 -0500

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @hilite = sort_by = params[:sort_by]
+    @movies = Movie.order(sort_by)
+
   end
 
   def new
